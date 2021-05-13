82a618f59e1302fb8fa7cbfd0cdc146ca1648dbb
thaoutpham <dongxuanhien65.dx@gmail.com>
2021-05-13 14:49:11 +0700

    Comparable

---
+++ src/ComparableCircleTest.java
@@ -0,0 +1,21 @@
+import java.util.Arrays;
+
+public class ComparableCircleTest {
+    public static void main(String[] args) {
+        ComparableCircle[] circles = new ComparableCircle[3];
+        circles[0] = new ComparableCircle("red", true, 3.5);
+
+        System.out.println("Pre-sorted:");
+        for (ComparableCircle circle : circles) {
+            System.out.println(circle);
+        }
+
+        Arrays.sort(circles);
+
+        System.out.println("After-sorted:");
+        for (ComparableCircle circle : circles) {
+            System.out.println(circle);
+        }
+    }
+}
+
